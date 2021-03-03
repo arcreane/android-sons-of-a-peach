404fdb6a908aacf0fb171806dc5ed523b64c9d50
seb830 <seb.flouvat@gmail.com>
2021-03-03 14:23:24 +0100

    main menu + activity game

--- app/src/main/java/fr/sons_of_a_peach/masterfruits/MainActivity.java
+++ app/src/main/java/fr/sons_of_a_peach/masterfruits/MainActivity.java
@@ -1,11 +1,15 @@
 package fr.sons_of_a_peach.masterfruits;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //on affecte l'attribut au bouton : id = play
-        this.play = (Button)findViewById(R.id.play);
+        this.play = (Button) findViewById(R.id.play);
 
         //on crée un listener au click du bouton
         play.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +41,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.exit:
+                Toast.makeText(this, "byebye", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/fr/sons_of_a_peach/masterfruits/game.java
+++ app/src/main/java/fr/sons_of_a_peach/masterfruits/game.java
@@ -1,8 +1,12 @@
 package fr.sons_of_a_peach.masterfruits;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class game extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.exit:
+                Toast.makeText(this, "byebye", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
